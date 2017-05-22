5dff980f9dc4001a52e42478b0cfcf32f6d909f3
RangiBecerra <rangibecerra@gmail.com>
2017-05-22 12:29:36 -0400

    desafio 5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 //alert("existe");
+var contenedor = document.getElementsByClassName("contenedor")[0];
+
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";
+
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
+
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
